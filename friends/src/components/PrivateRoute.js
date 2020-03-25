7733403445f16eadcfdfe27471cecb372d540545
dqxy <yanofsky.david@gmail.com>
2020-03-25 18:46:17 -0400

    private route

--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -9,25 +9,34 @@ import { Route, Redirect } from 'react-router-dom';
   3. It checks if the user is authenticated, if they are, it renders the “component” prop. If not, it redirects the user to /login.
 */
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  // fancy JS to pull the component prop out of the props obj
-  // use the ...rest operator
-  // rename component to Component
-  return (
-    <Route
-      {...rest}
-      render={props => {
-        if (localStorage.getItem('token')) {
-          // user is authed
-          return <Component {...props} />;
-        } else {
-          // user not authed - redirect to /login
-          return <Redirect to="/login" />;
-        }
-      }}
-    />
-  );
+// const PrivateRoute = ({ component: Component, ...rest }) => {
+//   // fancy JS to pull the component prop out of the props obj
+//   // use the ...rest operator
+//   // rename component to Component
+//   return (
+//     <Route
+//       {...rest}
+//       render={props => {
+//         if (localStorage.getItem('token')) {
+//           // user is authed
+//           return <Component {...props} />;
+//         } else {
+//           // user not authed - redirect to /login
+//           return <Redirect to="/login" />;
+//         }
+//       }}
+//     />
+//   );
+// };
+
+//better method:
+const PrivateRoute = ({ component, ...options }) => {  
+  const { user } = useAuthDataContext();  
+  const finalComponent = user ? component :   SignInPage;
+  return <Route {...options} component={finalComponent} />;
 };
+
+
 
 export default PrivateRoute;
 
